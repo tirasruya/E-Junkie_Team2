4c458dae6c92e555acceb5fe5812eb3eee175110
tirasruya <tirasruya@gmail.com>
2025-11-16 13:58:11 +0300

    Screenshot method updated

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -1,7 +1,6 @@
 package Hooks;
 
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,42 +14,42 @@ import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Hooks extends BaseDriver {
+public class Hooks {
 
-    public static WebDriver driver;
     public Logger LOGGER = LogManager.getLogger(this.getClass());
-
 
     @After
     public void tearDown(Scenario scenario) {
-        if (driver == null) return;
+
+        WebDriver driver = BaseDriver.getDriver();
 
         try {
-            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            String scenarioName = scenario.getName().replaceAll(" ", "_");
+            if (scenario.isFailed()) {
+                String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String scenarioName = scenario.getName().replaceAll(" ", "_");
 
-            String folderPath = System.getProperty("user.dir") + "/src/screenshots/";
-            File folder = new File(folderPath);
-            if (!folder.exists()) folder.mkdirs();
+                String folderPath = System.getProperty("user.dir") + "/src/screenshots/";
+                File folder = new File(folderPath);
+                if (!folder.exists()) folder.mkdirs();
 
-            TakesScreenshot ts = (TakesScreenshot) driver;
-            byte[] screenshotBytes = ts.getScreenshotAs(OutputType.BYTES);
+                byte[] screenshotBytes = ((TakesScreenshot) driver)
+                        .getScreenshotAs(OutputType.BYTES);
 
-            File destFile = new File(folderPath + scenarioName + "_" + timestamp + ".png");
-            try (FileOutputStream out = new FileOutputStream(destFile)) {
-                out.write(screenshotBytes);
+                File destFile = new File(folderPath + scenarioName + "_" + timestamp + ".png");
+                try (FileOutputStream out = new FileOutputStream(destFile)) {
+                    out.write(screenshotBytes);
+                }
+                scenario.attach(screenshotBytes, "image/png", "Failure Screenshot");
             }
 
-            scenario.attach(screenshotBytes, "image/png", "Failure Screenshot");
-
         } catch (Exception e) {
-            e.printStackTrace();
-            LOGGER.error("Screenshot alınamadı: {}", e.getMessage());
+            LOGGER.error("Screenshot failed: {}", e.getMessage());
         } finally {
             try {
                 driver.quit();
+                BaseDriver.driver = null;
             } catch (Exception e) {
-                LOGGER.error("Driver quit hatası: " + e.getMessage());
+                LOGGER.error("Driver quit error: " + e.getMessage());
             }
         }
     }
